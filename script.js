33c7c903a8c9d64961e0c4941e40f0eecd78787c
Benjamin <bhatfield120@gmail.com>
2022-05-11 18:57:07 +1000

    Added basic in browser UI using html, css and javascript
    Used the recent knowledge of event listeners and callback functions to create a basic
    UI for the scissors, paper , rock game already created for the console. I have decided
    to leave the 5 round functionality instead of the first to five points functionality
    mentioned on TOP as I enjoyed having the round counter on the screen and there being a
    fixed amount of rounds.

--- script.js
+++ script.js
@@ -1,158 +1,125 @@
-
-const choices = ["scissors", "paper", "rock"]
-
-//helper functions for round results
+//helper functions for Game results
 
 function win(num) {
   if (num === 1) {
-    console.log("Nice Work, you won");
+    return "Nice Work, you won";
   } else if (num === 2) {
-    console.log("Nice work you won the match")
-  }
-  
+    return " Nice work you won the match";
+  }  
 }
+
 function lose(num) {
   if (num === 1) {
-    console.log("Damn you lost this one");
+    return "Damn you lost this one";
   } else if (num === 2) {
-    console.log("You lost this match")
-  }
-  
+    return " You lost this match";
+  }  
 }
+
 function tie(val1, val2) {
-  console.log(("Thats a tie " + val1 + " VS " + val2));
+  return ("Thats a tie " + val1 + " VS " + val2);
 }
+
+//Helper functions for round results
 
 function RVS() {
-  console.log("Rock beats scissors.");
+  return "Rock beats scissors.";
 }
 function PVR() {
-  console.log("Paper beats rock.");
+  return "Paper beats rock.";
 }
 function SVP() {
-  console.log("Scissors beats paper.");
+  return "Scissors beats paper.";
 }
 
-//This function asks the player to enter their choice for 
-//Make this function fine to take null values
-
-function takeInput() {
-  let input = prompt("Please enter scissors, paper or rock?")
-  input = checkForNull(input);
-  while (input != "scissors" && input != "paper" && input != "rock") {
-    input = prompt("Please enter scissors, paper or rock?")
-  }
-  return input;
-}
-
-function checkForNull(input) {
-  if (input == null) {
-    return;
-  } else {
-    return input.toLowerCase();
-  }
-}
-//this function returns the computers random selection of scissors, 
-//paper or rock based on the choices array defined above and the random 
-//number generator producing a number between 0 and 2.
+//returns random selection of below array
 
 function computerPlay() {
-  return choices[Math.floor(Math.random() * 3)]
+  return ['scissors', 'paper', 'rock'][Math.floor(Math.random() * 3)]
 }
 
-//this function takes the two values picked by the computer and player and 
-//compares them based of the rules of scissors paper rock, then console logs 
-//the results of each match.
+//Updates round and score variables after each round
 
-function playRound(playerSection, computerSelection) {
-  console.log("You picked: " + playerSection + " The computer picked: " + computerSelection);
+function updateRoundScore(player, comp, round) {
+  document.getElementById('home-score').textContent = player;
+  document.getElementById('away-score').textContent = comp;
+  if (round > 5) {
+    document.getElementById('round').textContent = "FINISH";
+  } else {
+    document.getElementById('round').textContent = round;
+  }
+}
+
+//When roundCount reaches 6, checkWinner function decides the winner based
+//off final scores
+
+function checkWinner(playerScore, computerScore, resultPara) {
+  if (playerScore > computerScore) {
+    resultPara.textContent += win(2); 
+  } else if (computerScore > playerScore) {
+    resultPara.textContent += lose(2);
+  } else {
+    resultPara.textContent += tie(playerScore, computerScore);
+  }
+}
+
+//takes input variable and compares to computer selection
+//decides winner and returns number to update scoring 0 = tie, 1 = win, 2 = lose
+
+function playRound(playerSection, computerSelection, resultPara) {
+  resultPara.textContent = '';
   if (playerSection == computerSelection) {
-    tie(playerSection, computerSelection);
+    resultPara.textContent = tie(playerSection, computerSelection);
     return 0
   } else if (playerSection == "scissors" && computerSelection == "paper") {
-    win(1);
-    SVP();
+    resultPara.textContent = `${win(1)}, ${SVP()}`;
     return 1;
   } else if (playerSection == "scissors" && computerSelection == "rock") {
-    lose(1);
-    RVS();
+    resultPara.textContent = `${lose(1)}, ${RVS()}`;
     return 2;
   } else if (playerSection == "rock" && computerSelection == "scissors") {
-    win(1);
-    RVS();
+    resultPara.textContent = `${win(1)}, ${RVS()}`;
     return 1;
   } else if (playerSection == "rock" && computerSelection == "paper") {
-    lose(1);
-    PVR();
+    resultPara.textContent = `${lose(1)}, ${PVR()}`;
     return 2;
   } else if (playerSection == "paper" && computerSelection == "rock") {
-    win(1);
-    PVR();
+    resultPara.textContent = `${win(1)}, ${PVR()}`;
     return 1;
   } else if (playerSection == "paper" && computerSelection == "scissors") {
-    lose(1);
-    SVP();
+    resultPara.textContent = `${lose(1)}, ${SVP()}`;
     return 2;
   }
 }
+
+//sets all base variables and listens for button clicks
 
 function game() {
   let playerScore = 0;
   let computerScore = 0;
-  let roundCount = 0;
-
-  while (roundCount < 5) {
-    let result = playRound(takeInput(), computerPlay());
+  let roundCount = 1;
+  let resultPara = document.getElementById('result-box')
+  resultPara.textContent = 'Select a choice from below';
+  updateRoundScore(playerScore, computerScore, roundCount);
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach(button => button.addEventListener('click', input));
+  function input(e) {
+    let result = playRound(this.classList.value, computerPlay(), resultPara);
     if (result == 1) {
-      playerScore++;
-      roundCount++;
-      console.log("Home: " + playerScore + " Away: " + computerScore + " Round: " + roundCount);
+      playerScore++; roundCount++;
+      updateRoundScore(playerScore, computerScore, roundCount);
     } else if (result == 2) {
-      computerScore++;
-      roundCount++;
-      console.log("Home: " + playerScore + " Away: " + computerScore + " Round: " + roundCount);
+      computerScore++; roundCount++;
+      updateRoundScore(playerScore, computerScore, roundCount);
     } else {
       roundCount++;
-      console.log("Home: " + playerScore + " Away: " + computerScore + " Round: " + roundCount);
+      updateRoundScore(playerScore, computerScore, roundCount);
     }
-  }
-  checkWinner(playerScore, computerScore);
-  loopGame(2);
-}
-
-function checkWinner(playerScore, computerScore) {
-  if (playerScore > computerScore) {
-    win(2); 
-  } else if (computerScore > playerScore) {
-    lose(2);
-  } else {
-    tie(playerScore, computerScore);
-  }
-}
-
-function loopGame(num) {
-  if (num === 1) {
-    let decision = prompt("Would you like to play Scissors, Paper, Rock?");
-    decision = checkForNull(decision);
-    if (decision == "yes") {
-      game();
-    } else {
-      console.log("Thanks for coming!!");
+    if (roundCount == 6) {
+      checkWinner(playerScore, computerScore, resultPara);
+      setTimeout( () => game(), 2000);      
     } 
-  } else if (num == 2) {
-    let loopCounter = true;
-    while (loopCounter) {
-      let decision = prompt("Would you like to play again?")
-      decision = checkForNull(decision);
-      if (decision === "yes") {
-        game()
-      } else {
-        console.log("Thanks for playing!")
-        loopCounter = false;
-        break;
-      }
-    }
   }
 }
 
-loopGame(1);
+game();
